f4b830fafcecbcf15080808812035636f99de1a9
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-04-29 15:25:07 -0700

    Update diskuv repository
    Includes:
    * Relax constraints on ocaml-system + dkml-base-compiler

--- .github/workflows/scripts/localdev/windows_vars.source.sh
+++ .github/workflows/scripts/localdev/windows_vars.source.sh
@@ -11,7 +11,7 @@ fi
 export bootstrap_opam_version="0.0.0"
 export opam_abi=windows_x86 #_64
 
-export DEFAULT_DISKUV_OPAM_REPOSITORY_TAG=2ad0ad744d7a85c5a1651d9b0b254590096cf50c # "v0.4.0-prerel19_r5"
+export DEFAULT_DISKUV_OPAM_REPOSITORY_TAG=47f860c6596b8a8f6237084472e9f7ce0026b02e # "v0.4.0-prerel19_r5"
 export PIN_OCAMLFIND=1.9.1
 export PIN_OCAMLBUILD=0.14.0
 export INSTALLERNAME=ocaml
