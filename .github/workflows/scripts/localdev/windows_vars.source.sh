5dba5c979b1f4b7bc25e19280499540008de71ac
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-05-04 21:33:22 -0700

    Add patch for digestif.1.0.1

--- .github/workflows/scripts/localdev/windows_vars.source.sh
+++ .github/workflows/scripts/localdev/windows_vars.source.sh
@@ -11,7 +11,7 @@ fi
 export bootstrap_opam_version="0.0.0"
 export opam_abi=windows_x86 #_64
 
-export DEFAULT_DISKUV_OPAM_REPOSITORY_TAG=27548fbe5107c87a843f70eede3f0564056ecb92 # "v0.4.0-prerel19_r7"
+export DEFAULT_DISKUV_OPAM_REPOSITORY_TAG=c97b448b45bbfcaa22aa370e9a2efa50752edfc1 # "v0.4.0-prerel20_r2"
 export PIN_OCAMLFIND=1.9.1
 export PIN_OCAMLBUILD=0.14.0
 export INSTALLERNAME=ocaml
