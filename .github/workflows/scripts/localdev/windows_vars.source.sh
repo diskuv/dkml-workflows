7a4a49c5ab5e8fbd2757f13955c597e082feaed2
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2022-06-01 16:20:44 -0700

    Include bigstringaf.0.9.0+msvc

--- .github/workflows/scripts/localdev/windows_vars.source.sh
+++ .github/workflows/scripts/localdev/windows_vars.source.sh
@@ -11,7 +11,7 @@ fi
 export bootstrap_opam_version="0.0.0"
 export opam_abi=windows_x86 #_64
 
-export DEFAULT_DISKUV_OPAM_REPOSITORY_TAG=f2329c329aa2497808cd2ffa759b696c6cdf96dd
+export DEFAULT_DISKUV_OPAM_REPOSITORY_TAG=a56ac3d7131f98b4a10dc6422bcd777c3e54d478
 export PIN_OCAMLFIND=1.9.1
 export PIN_OCAMLBUILD=0.14.0
 export INSTALLERNAME=ocaml
