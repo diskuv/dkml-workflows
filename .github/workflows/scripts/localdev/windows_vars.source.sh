e7d0e7a2fb6d9ce4c20c55fe1911b7d78569cfae
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2022-07-07 15:13:21 -0700

    diskuv-opam-repository#0c939d8000af66f613ecc1a7ad81f6711d68437b

--- .github/workflows/scripts/localdev/windows_vars.source.sh
+++ .github/workflows/scripts/localdev/windows_vars.source.sh
@@ -11,7 +11,7 @@ fi
 export bootstrap_opam_version="0.0.0"
 export opam_abi=windows_x86 #_64
 
-export DEFAULT_DISKUV_OPAM_REPOSITORY_TAG=334eb2ed3e6fac2784419361bde3523a2a99e043
+export DEFAULT_DISKUV_OPAM_REPOSITORY_TAG=0c939d8000af66f613ecc1a7ad81f6711d68437b
 export PIN_OCAMLFIND=1.9.1
 export PIN_OCAMLBUILD=0.14.0
 export INSTALLERNAME=ocaml
